d6452dddbd92598e3bd09bcc66b567922f8a7976
Kerie <ogedegbekerieo@gmail.com>
2024-05-14 23:29:12 +0100

    two

--- src/components/currency-converter.jsx
+++ src/components/currency-converter.jsx
@@ -96,6 +96,7 @@ const CurrencyConverter = () => {
           className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 mt-1"
         />
       </div>
+      {/* now! */}
       <div className="flex justify-end mt-6">
         <button
           onClick={convertCurrency}
